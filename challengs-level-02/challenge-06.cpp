2ddb4be236c051c322d5633a3bc93641f49430e6
Alexandro Abade <alexandroabade@gmail.com>
2022-09-23 01:17:32 -0300

    Challenge 06 completed, now it's time to factor

--- challengs-level-02/challenge-06.cpp
+++ challengs-level-02/challenge-06.cpp
@@ -1,5 +1,34 @@
-# Crie um Algoritmo em C++, em que o usuário informa os gastos com alimentação de 5 famílias, calcula 
-média de gastos e diz de cada família, se seu gasto é baixo da média, acima da média ou na média.
+#include <iostream>
+using namespace std;
+
+int main(){
+	float familia1, familia2, familia3, familia4, familia5;
+	
+	cout << "Por favor, informe os gastos da primeira família: ";
+	cin >> familia1; 
+	cout << "Por favor, informe os gastos da segunda família: ";
+	cin >> familia2; 
+	cout << "Por favor, informe os gastos da terceira família: ";
+	cin >> familia3;
+	cout << "Por favor, informe os gastos da quarta família: ";
+	cin >> familia4;
+	cout << "Por favor, informe os gastos da quinta família: ";
+	cin >> familia5;
+
+	float mediaCadaFamilia = (familia1 + familia2 + familia3 + familia4 + familia5);
+	float media = mediaCadaFamilia / 5;
 
 
+	if (mediaCadaFamilia < media){
+		cout << ">> O gasto da família está abaixo da média. MÉDIA: " << media << endl;
+		cout << endl;
+	} else if (mediaCadaFamilia > media){	
+		cout << ">> O gasto da família está abaixo da média. MÉDIA: " << media << endl;
+		cout << endl;
+	} else {
+		cout << ">> O gasto da família está na média" << media << endl;
+		}
+		
+	return 0;
+}
 
