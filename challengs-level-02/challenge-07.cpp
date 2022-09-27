3e421a658a6354c70ef733d1fa2d69b6f8ab7c8b
Alexandro Abade <alexandroabade@gmail.com>
2022-09-26 22:22:14 -0300

    add changes to the challenge-7.cpp at line comment start main

--- challengs-level-02/challenge-07.cpp
+++ challengs-level-02/challenge-07.cpp
@@ -1,1 +1,10 @@
-Crie um Algoritmo em C++, em que o usuário informa a dimensão de uma matriz, e os elementos de duas matrizes (ambas com a mesma dimensão) e o Algoritmo mostra a soma entre estas duas matrizes.
+// Crie um Algoritmo em C++, em que o usuário informa a dimensão de uma matriz, e os elementos de duas matrizes [ambas com a mesma dimensão] e o Algoritmo mostra a soma entre estas duas matrizes.
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  return 0;
+}
