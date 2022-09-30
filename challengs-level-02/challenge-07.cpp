c209b2b32ab11c3ff10ad6d16f88fe6c9c684559
Alexandro Abade <alexandroabade@gmail.com>
2022-09-29 23:59:43 -0300

    Completed

--- challengs-level-02/challenge-07.cpp
+++ challengs-level-02/challenge-07.cpp
@@ -5,6 +5,43 @@
 using namespace std;
 
 int main(){
+  int numeroDeLinhas, numeroDeColunas;
+  cout << "Qual é a quantidade de linhas?\n";
+  cin >> numeroDeLinhas;
+  cout << "Qual é a quantidade de colunas";
+  cin >> numeroDeColunas;
+
+  float matriz1[numeroDeLinhas][numeroDeColunas], matriz2[numeroDeLinhas][numeroDeColunas];
+  cout << "Digite abaixo os elementos da matriz 1: \n";
+  for (int i=0; i<numeroDeLinhas; i++){
+    for (int j=0; j<numeroDeColunas; j++){
+      cout << "Digite o elemento da linha " << " coluna " << j+1 << endl;
+      cin >> matriz1[i][j];
+    }
+  }
+
+  cout << "Digite abaixo os elementos da matriz 2: \n";
+  for (int i=0; i<numeroDeLinhas; i++){
+    for (int j=0; j<numeroDeColunas; j++){
+      cout << "Digite o elemento da linha " << " coluna " << j+1 << endl;
+      cin >> matriz2[i][j];
+    }
+  }
+
+  float matrizSoma[numeroDeLinhas][numeroDeColunas];
+  for (int i=0; i<numeroDeLinhas; i++){
+    for (int j=0; j<numeroDeColunas; j++){
+      matrizSoma[i][j] = matriz[i][j] + matriz2[i][j];
+    }
+  }
+
+  for (int i=0; i<numeroDeLinhas; i++){
+    for (int j=0; j<numeroDeColunas; i++){
+      cout << matrizSoma[i][j] << " ";
+    }
+    cout << endl;
+
+  }
 
   return 0;
 }
